72b6349e8be41b3a1936e0d1b522245b84fdbcdd
Luna3152 <ehji5532@gmail.com>
2026-09-18 23:46:53 +0900

    Add directory PNG processing

--- src/delete_exif/processor.py
+++ src/delete_exif/processor.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 
-from PIL import Image
-from PIL.PngImagePlugin import PngInfo
+from .metadata import remove_metadata
 
 delete_keys = {"Description", "Software", "Source", "Generation time", "Comment"}
 
 
 def process_directory(
-        input_dir: Path,
-        output_dir: Path,
-        delete_keys: str[str],
-        ) -> None:
-    pass
+    input_dir: Path,
+    output_dir: Path,
+    delete_keys: str[str],
+) -> None:
+
+    for input_path in input_dir.glob("*.png"):
+        output_path = output_dir / input_path.name
+
+        remove_metadata(input_path=input_path, output_path=output_path)
